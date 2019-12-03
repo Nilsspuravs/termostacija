bc07e8f678362068d06d586ea93896bc04da4890
Nils <spuravs85@gmail.com>
2019-12-03 09:24:35 +0200

    Update termo.py

--- termo.py
+++ termo.py
@@ -27,9 +27,7 @@ a = 1
 
    
 while True:
-        button_state1 = GPIO.input(4) #Ja poga ir inputs true
-        button_state2 = GPIO.input(18)
-        button_state3 = GPIO.input(23)
+        a = 1
         try:
                 print("Ja velies sanemt datus nospied -c-") #printejam,lai zinatu.
                 if button_state1 == GPIO.LOW: #Ja nospiez pogu, daram to
